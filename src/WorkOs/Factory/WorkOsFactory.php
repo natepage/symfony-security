6c4d9ccd3b1104d22a4a5e02f4a5d46b8275f381
natepage <nathan.page86@gmail.com>
2026-06-29 03:23:35 +0000

    [SymfonySecurity] Add logging to understand better when WorkOsFactory is called

--- src/WorkOs/Factory/WorkOsFactory.php
+++ src/WorkOs/Factory/WorkOsFactory.php
@@ -3,12 +3,23 @@ declare(strict_types=1);
 
 namespace NatePage\SymfonySecurity\WorkOs\Factory;
 
+use Psr\Log\LoggerInterface;
 use WorkOS\WorkOS;
 
 final readonly class WorkOsFactory
 {
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+    }
+
     public function create(string $apiKey, string $clientId): WorkOS
     {
+        $this->logger->debug('Creating WorkOS instance', [
+            'apiKey' => $apiKey,
+            'clientId' => $clientId,
+        ]);
+
         return new WorkOS($apiKey, $clientId);
     }
 }
